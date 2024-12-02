0abf62be224bba1937096fd54d7ad9dd2130bfce
Lucrecia-15 <parodilucrecia@gmail.com>
2024-12-02 19:34:04 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+};
+// Declaramos una función con el nombre exacto "fromDollarToYen"
+const fromDollarToYen = function(valueInDollar) {
+    // Convertimos el valor a Yen
+    let valueInYen = valueInDollar * 156.5;
+    // Retornamos el valor en Yen
+    return valueInYen;
+};
+// Declaramos una función con el nombre exacto "fromYenToPound"
+const fromYenToPound = function(valueInYen) {
+    // Convertimos el valor a Pound
+    let valueInPound = valueInYen * 0.87;
+    // Retornamos el valor en Pound
+    return valueInPound;
+};
+const sum = (a,b) => {
+   return a + b} 
+console.log(sum(7,3))
+
+
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+module.exports = {sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
